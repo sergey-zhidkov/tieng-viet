0acc362fdd4e342970e3c4acc4b6f06594209247
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2025-05-25 04:10:54 -0700

    remove dist

--- dist/js/options.js
+++
@@ -1,6 +0,0 @@
-/*
- Zhongwen - A Chinese-English Pop-Up Dictionary
- Copyright (C) 2010-2019 Christian Schiller
- https://chrome.google.com/extensions/detail/kkmlkkjojmombglmlpbpapmhcaljjkde
- */
-'use strict';
